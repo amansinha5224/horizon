6b9a602fefc066b5cf7e9de27d1cb84eb7769365
Naman Sinha <aman.sinha5224@gmail.com>
2025-09-04 11:48:38 +0530

    add contact form update notification

--- Horizon/home/views.py
+++ Horizon/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -15,13 +16,17 @@ def contact(request):
         email = request.POST['email']
         content = request.POST['content']
 
-        contact = Contact(
-            name = name,
-            phone = phone,
-            email = email,
-            content = content,
-        )
+        if(len(name) < 2 or len(email) < 5 or len(phone) < 10 or len(content) < 2):
+            messages.error(request, "Please fill the form correctly!")
+        else:
+            messages.success(request, "Form submitted successfully!")
+            contact = Contact(
+                name = name,
+                phone = phone,
+                email = email,
+                content = content,
+            )
 
-        contact.save()
+            contact.save()
         
     return render(request, 'home/contact.html')
